3f84cf9ca3605f768b40250ffbb217a8f6c07af1
Gautam Ramasubramanian <gautam.ramasubramanian@macaulay.cuny.edu>
2024-06-27 13:46:05 -0400

    Moved Header/Footer components to root layout.tsx file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,49 @@
 "use client";
 
+import Link from "next/link";
 import "./globals.css";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+
+function Header() {
+  return (
+    <div className="h-[5dvh] flex flex-row bg-[#0d1117] justify-between border-b">
+      <Link
+        href="/"
+        className="flex flex-col justify-center px-8 hover:cursor-pointer hover:bg-[#24292f] hover:font-bold"
+      >
+        <p className="text-lg">ToDo App</p>
+      </Link>
+      <div className="flex flex-row">
+        <Link
+          href="/signup"
+          className="flex flex-col justify-center px-8 hover:cursor-pointer hover:bg-[#24292f] hover:font-bold"
+        >
+          <p>Sign Up</p>
+        </Link>
+        <Link
+          href="/login"
+          className="flex flex-col justify-center px-8 hover:cursor-pointer hover:bg-[#24292f] hover:font-bold"
+        >
+          <p>Login</p>
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+function Footer() {
+  return (
+    <div className="h-[5dvh] flex justify-end bg-[#0d1117] border-t">
+      <a
+        href="https://github.com/gramasu000"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="px-8 flex flex-col justify-center hover:cursor-pointer hover:bg-[#24292f] hover:font-bold"
+      >
+        <p>Gautam Ramasubramanian</p>
+      </a>
+    </div>
+  );
+}
 
 export default function RootLayout({
   children,
